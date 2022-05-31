0d7dfd550688e712e337276a3b3a5d12d6212a20
Mohammad Rasoul <mrn2277@gmail.com>
2022-05-31 11:39:10 +0430

    minor improvements

--- hours/sheets/admin.py
+++ hours/sheets/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from sheets.models import *
 
-# Register your models here.
-admin.site.register(Project)
+admin.site.site_url = "/hours"
+
+admin.site.unregister(Group)
+
 admin.site.register(ProjectFamily)
 admin.site.register(Sheet)
 
-admin.site.unregister(Group)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    ordering = ['family__name']
