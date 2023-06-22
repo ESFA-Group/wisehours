29d93c5195238d93906efa5918a5f7ffa79b1207
Mohammad Rasoul <mrn2277@gmail.com>
2023-06-22 19:20:52 +0330

    backup shell update

--- hours/db-backup.sh
+++ hours/db-backup.sh
@@ -6,5 +6,9 @@ _dow="$(date +'%A')"
 # open database, wait up to 1 seconds for any activity to end and create a backup file
 sqlite3 db.sqlite3 << EOF
 .timeout 1000
-.backup /backups/db_${_dow}.sqlite3
+.backup db_${_dow}.sqlite3
 EOF
+
+gdrive files upload --parent 1JbFc6ECZQmWuDgzXF3ZQ7VCE7HxJwdDO db_${_dow}.sqlite3
+
+rm db_${_dow}.sqlite3
