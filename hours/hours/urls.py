51f625a4ee4b4f88301fe2588d05d7a6b8af5ac2
amin-s81 <amin.sanei81@gmail.com>
2024-06-09 17:10:30 +0330

    fix urls redirection

--- hours/hours/urls.py
+++ hours/hours/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.conf import settings
 
 urlpatterns = [
     path("hours/admin/", admin.site.urls),
     path("hours/api-auth/", include("rest_framework.urls")),
     path("hours/", include("sheets.urls")),
+    path("", RedirectView.as_view(url="hours", permanent=True)),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
