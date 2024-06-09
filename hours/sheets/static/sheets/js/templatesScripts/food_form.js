7ad2616f5cac8644ad0464b95b2f2f361a49e453
amin-s81 <amin.sanei81@gmail.com>
2024-06-09 17:10:30 +0330

    feat: fill food table

--- hours/sheets/static/sheets/js/templatesScripts/food_form.js
+++ hours/sheets/static/sheets/js/templatesScripts/food_form.js
@@ -1,26 +1,77 @@
+//CONSTANTS************************************************
+// const TODAY = new JDate(1403, 2, 22);
+const TODAY = new JDate();
+
+const CURRENT_YEAR = TODAY.getFullYear();
+var ACTIVE_YEAR = CURRENT_YEAR;
+
+const CURRENT_MONTH = TODAY.getMonth();
+var ACTIVE_MONTH = CURRENT_MONTH
+
+const CURRENT_MONTH_WEEKS = getWeeksOfMonth()
+var ACTIVE_MONTH_WEEKS = CURRENT_MONTH_WEEKS
+
+const [CURRENT_WEEK, CURRENT_WEEK_INDEX] = getCurrentWeek()
+var ACTIVE_WEEK = CURRENT_WEEK
+var ACTIVE_WEEK_INDEX = CURRENT_WEEK_INDEX
+
+
+
+
 foods = [
 	{
+		num: 0,
+		name: "باقالی پلو",
+		price: 1400000,
+	},
+	{
+		num: 1,
 		name: "چلو کباب کوبیده",
 		price: 1400000,
 	},
 	{
+		num: 2,
 		name: "چلو جوجه کباب",
 		price: 1400000,
 	},
 	{
+		num: 3,
 		name: "چلو قیمه",
 		price: 1400000,
 	},
 	{
+		num: 4,
 		name: "چلو قورمه",
 		price: 1400000,
 	},
 	{
+		num: 5,
 		name: "زرشک پلو با مرغ",
 		price: 1400000,
 	},
 	{
+		num: 6,
 		name: "خوراک",
+		price: 1400000
+	},
+	{
+		num: 7,
+		name: "الویه",
+		price: 1400000
+	},
+	{
+		num: 8,
+		name: "کشک بادمجان",
+		price: 1400000
+	},
+	{
+		num: 9,
+		name: "ماست",
+		price: 1400000
+	},
+	{
+		num: 10,
+		name: "زیتون",
 		price: 1400000
 	},
 ]
@@ -34,23 +85,27 @@ var weekdays = [
 	'پنج‌شنبه',
 	'جمعه',
 ]
+// ********************************************************
 
-
-function fillYears(year) {
-	for (let i = window.START_YEAR; i <= year; i++) {
+function fillYears() {
+	for (let i = window.START_YEAR; i <= ACTIVE_YEAR; i++) {
 		$("#year").append($("<option>").text(i));
 	}
 }
 
-function fillWeeks(year, month) {
-	let weeks = getWeeksOfMonth(year, month)
+function fillWeeks() {
+	let weeks = getWeeksOfMonth()
 	$("#week").empty();
 	for (let i = 0; i < weeks.length; i++) {
-		$("#week").append($("<option>").text(`week${i + 1} (${weeks[i]['0'].format('MM/DD')} --> ${weeks[i]['6'].format('MM/DD')})`));
+		$("#week").append($("<option>")
+			.text(`week${i + 1} (${weeks[i]['0'].format('MM/DD')} --> ${weeks[i]['6'].format('MM/DD')})`)
+			.val(i));
 	}
 }
 
-function getWeeksOfMonth(year, month) {
+function getWeeksOfMonth() {
+	let year = ACTIVE_YEAR
+	let month = ACTIVE_MONTH
 	const totalDaysInMonth = JDate.daysInMonth(year, month);
 
 	let weeksDate = []
@@ -65,7 +120,7 @@ function getWeeksOfMonth(year, month) {
 				0: startDate
 			})
 			for (let j = 1; j < 7; j++) {
-				i = i+1
+				i = i + 1
 				if (i > totalDaysInMonth) {
 					month += 1;
 					if (month > 12) {
@@ -87,7 +142,22 @@ function getWeeksOfMonth(year, month) {
 	return weeksDate;
 }
 
-function fillFoodTable() {
+function getCurrentWeek() {
+	for (const [index, week] of Object.entries(CURRENT_MONTH_WEEKS)) {
+		let startweekday = week[0]
+		dayDiff = getDayDiff(TODAY, startweekday)
+		if (dayDiff <= 0 && dayDiff >= -6) {
+			return [week, index];
+		}
+	};
+}
+
+function getDayDiff(JDate1, JDate2) {
+	diff = JDate2._d.getTime() - JDate1._d.getTime()
+	return Math.round(diff / (1000 * 60 * 60 * 24));
+}
+
+function initializeFoodTable() {
 	$("#foodTable tbody").empty();
 	$("#foodTable thead tr").remove();
 
@@ -106,11 +176,11 @@ function fillFoodTableHeader() {
 	$("#foodTable thead").append(headersRow);
 }
 function fillFoodTablebody() {
-	weekdays.forEach(day => {
+	for (const [index, day] of Object.entries(ACTIVE_WEEK)) {
 		var row = $("<tr>");
 
 		// Add day cell
-		row.append($("<td>").text(day));
+		row.append($("<td>").text(day.format("dddd")).val(day.date[2]));
 
 		foods.forEach(food => {
 			var cellContent = $("<span>");
@@ -122,19 +192,80 @@ function fillFoodTablebody() {
 		})
 
 		$("#foodTable tbody").append(row);
-
-	});
+	}
 }
 
-function getSelectedFoods() {
+function fillFoodTable() {
+
+	// const food_data = getFoodData(ACTIVE_YEAR, ACTIVE_MONTH)
+	// const food_data = [
+	// 	{ "day": 1, "foods": [0, 9] },
+	// 	{ "day": 2, "foods": [1, 8] },
+	// 	{ "day": 3, "foods": [5, 8, 9] },
+	// 	{ "day": 6, "foods": [7, 9] },
+	// 	{ "day": 7, "foods": [4] },
+	// 	{ "day": 11, "foods": [1, 8] },
+	// 	{ "day": 12, "foods": [2, 8] },
+	// 	{ "day": 13, "foods": [3, 9] },
+	// 	{ "day": 16, "foods": [0, 10] }
+	// ]
+	const food_data = [
+		[
+			{ "day": 5, "foods": [0, 9] },
+			{ "day": 6, "foods": [5, 8, 9] },
+			{ "day": 7, "foods": [1, 8] },
+			{ "day": 9, "foods": [7, 9] }
+		],
+		[
+			{ "day": 12, "foods": [0, 9] },
+			{ "day": 13, "foods": [2] },
+			{ "day": 14, "foods": [5, 8, 10] },
+			{ "day": 16, "foods": [7, 9] }
+		],
+		[
+			{ "day": 19, "foods": [1, 8] },
+			{ "day": 20, "foods": [2, 8] },
+			{ "day": 21, "foods": [3, 9] },
+			{ "day": 22, "foods": [0, 10] },
+			{ "day": 25, "foods": [0, 10] }
+		],
+		[
+			{ "day": 26, "foods": [0, 9] },
+			{ "day": 27, "foods": [2, 8] },
+			{ "day": 28, "foods": [3] },
+			{ "day": 1, "foods": [0, 10] }
+		]
+	]
+
+	activeWeek_food_data = food_data[ACTIVE_WEEK_INDEX]
+	let rows = $('#foodTable tr')
+
+	activeWeek_food_data.forEach(dayfood => {
+		const matchingRow = rows.filter((i, r) => r.cells[0].value == dayfood.day)[0];
+		if (matchingRow !== undefined) {
+			[day, ...checkboxes] = matchingRow.cells;
+			for (const food of dayfood.foods) {
+				var checkbox = checkboxes[food].querySelector('input[type="checkbox"]');
+				checkbox['checked'] = true;
+				continue;
+			}
+		}
+	});
+
+}
+
+function submitSelectedFoods() {
 	selectedFood = []
 	selectedFood = [
-		{ day: 0, selectedFoods: [0] },
-		{ day: 1, selectedFoods: [1, 2] },
-		{ day: 2, selectedFoods: [5] },
-		{ day: 3, selectedFoods: [] },
-		{ day: 4, selectedFoods: [4] },
-		{ day: 6, selectedFoods: [1, 5] },
+		{ "day": 1, "foods": [0, 9] },
+		{ "day": 2, "foods": [1, 8] },
+		{ "day": 3, "foods": [5, 8, 9] },
+		{ "day": 6, "foods": [7, 9] },
+		{ "day": 7, "foods": [4] },
+		{ "day": 11, "foods": [1, 8] },
+		{ "day": 12, "foods": [2, 8] },
+		{ "day": 13, "foods": [3, 9] },
+		{ "day": 16, "foods": [0, 10] }
 	]
 
 	const table = $('#foodTable')[0]
@@ -148,21 +279,35 @@ function getSelectedFoods() {
 	});
 }
 
+function handleChangeWeek() {
+	ACTIVE_WEEK_INDEX = $("#week").val();
+	ACTIVE_WEEK = ACTIVE_MONTH_WEEKS[ACTIVE_WEEK_INDEX];
+	initializeFoodTable()
+	fillFoodTable();
+}
+
 $("document").ready(async function () {
-	const today = new JDate();
-	const currentYear = today.getFullYear();
-	const currentMonth = today.getMonth();
-	fillYears(currentYear);
-	fillWeeks(currentYear, currentMonth);
+
+	fillYears(ACTIVE_YEAR);
+	fillWeeks();
+	initializeFoodTable()
 	fillFoodTable()
-	$("#year").val(currentYear);
-	$("#month").val(currentMonth);
-	$("#current-sheet-date").text(`${currentYear}/${currentMonth}`);
+	$("#year").val(ACTIVE_YEAR);
+	$("#month").val(ACTIVE_MONTH);
+	$("#week").val(CURRENT_WEEK_INDEX);
+
+	$("#current-sheet-date").text(`${ACTIVE_YEAR}/${ACTIVE_MONTH}`);
 
 
 
 	$("#year, #month").change(function () {
-		fillWeeks($("#year").val(), $("#month").val());
+		ACTIVE_YEAR = $("#year").val()
+		ACTIVE_MONTH = $("#month").val()
+		fillWeeks();
+		handleChangeWeek()
+	});
+	$("#week").change(function () {
+		handleChangeWeek();
 	});
 
 	$('.cell-checkbox').parent().parent().click(function (e) {
@@ -174,7 +319,7 @@ $("document").ready(async function () {
 	});
 
 	$("#submitFood").on("click", function () {
-		selectedFoodObject = getSelectedFoods()
+		selectedFoodObject = submitSelectedFoods()
 		alert("Button clicked!");
 	});
 });
